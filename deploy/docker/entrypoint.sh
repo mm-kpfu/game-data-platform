5c3c335a138711340746b0c414d51c4181796e4d
Muzafarov Marsel <muzafarov.olymp@mail.ru>
2024-06-16 23:34:11 +0300

    add tf module

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -2,7 +2,25 @@
 
 /old-docker-entrypoint.sh "$@" &
 
-sleep 20
+FLINK_PID=$!
+
+URL="http://localhost:8081/config"
+
+INTERVAL=2
+
+check_server() {
+  STATUS=$(curl -s -o /dev/null -w '%{http_code}' $URL)
+  echo "HTTP Status: $STATUS"
+  if [ "$STATUS" -eq 200 ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+while ! check_server; do
+  sleep $INTERVAL
+done
 
 flink run examples/streaming/WordCount.jar &
 flink run examples/streaming/WindowJoin.jar &
@@ -10,3 +28,6 @@ flink run examples/streaming/TopSpeedWindowing.jar &
 flink run examples/streaming/SessionWindowing.jar &
 flink run examples/streaming/Iteration.jar &
 flink run examples/streaming/StateMachineExample.jar &
+
+
+wait $FLINK_PID
