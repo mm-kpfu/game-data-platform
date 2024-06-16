eabb7d1f3279edaac4951f7a501a1b23abf45945
Muzafarov Marsel <muzafarov.olymp@mail.ru>
2024-06-16 20:54:59 +0300

    add tf module

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -1,10 +1,9 @@
-echo "FROM ENTRYPOINT" && \
-/docker-entrypoint.sh && \
-echo "AFTER DOCKER-ENTRYPOINT" && \
+#!/bin/sh
+
+/old-docker-entrypoint.sh && \
 flink run examples/streaming/WordCount.jar & \
 flink run examples/streaming/WindowJoin.jar & \
 flink run examples/streaming/TopSpeedWindowing.jar & \
 flink run examples/streaming/SessionWindowing.jar & \
 flink run examples/streaming/Iteration.jar & \
-flink run examples/streaming/StateMachineExample.jar &&
-echo "AFTER ALL"
+flink run examples/streaming/StateMachineExample.jar
